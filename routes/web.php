4d5cc0deca8d8b142833e808a55ad3036fea92cd
nexuusdev <mamefat2004@gmail.com>
2026-09-05 08:46:55 +0000

    is a jour de mon routeur

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 use App\Controllers\HomeController;
 
 /** @var \Core\Router $router */
-$router->get('/', [HomeController::class, 'index']);
 use App\Controllers\AuthController;
 use App\Controllers\CategorieController;
 use App\Controllers\ProduitController;
+use App\Controllers\CommandeController;
+
+
+$router->get('/', [HomeController::class, 'index']);
 
 $router->get('/connexion', [AuthController::class, 'showLogin']);
 $router->post('/connexion', [AuthController::class, 'login']);
@@ -25,3 +28,10 @@ $router->get('/produits', [ProduitController::class, 'index']);
 $router->get('/produits/{id}', [ProduitController::class, 'show']);
 $router->post('/produits', [ProduitController::class, 'store'], ['auth', 'role:GERANT,ADMIN']);
 $router->post('/produits/{id}/delete', [ProduitController::class, 'delete'], ['auth', 'role:GERANT,ADMIN']);
+
+$router->post('/commandes', [CommandeController::class, 'store'], ['auth', 'role:CLIENT']);
+$router->get('/mes-commandes', [CommandeController::class, 'mesCommandes'], ['auth', 'role:CLIENT']);
+$router->get('/commandes', [CommandeController::class, 'index'], ['auth', 'role:GERANT,ADMIN']);
+$router->get('/commandes/{id}', [CommandeController::class, 'show'], ['auth']);
+$router->post('/commandes/{id}/statut', [CommandeController::class, 'changerStatut'], ['auth', 'role:GERANT,ADMIN']);
+$router->post('/commandes/{id}/annuler', [CommandeController::class, 'annuler'], ['auth']);
