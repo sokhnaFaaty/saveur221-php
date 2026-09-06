142f5060938ea8afbd3f3b27c82c1b4059fd1784
nexuusdev <mamefat2004@gmail.com>
2026-09-06 02:27:58 +0000

    ajout de lien de la librairie de fontawesome

--- views/layout/public.php
+++ views/layout/public.php
@@ -10,6 +10,7 @@ $user = $_SESSION['user'] ?? null;
     <title><?= isset($title) ? htmlspecialchars((string) $title) . ' - ' : '' ?>Saveur 221</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
